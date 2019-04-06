31d20222918214ef41af4928e493a6b69d2f8c3a
ytyaru <ytyaru@outlook.jp>
2019-04-07 07:01:05 +0900

    '${!...}'追記

--- src/a.sh
+++ src/a.sh
@@ -16,31 +16,39 @@ Reference() {
 	#for item in ${ARRAY[@]}; do echo "$item"; done;
 	echo "Count: ${#ARRAY[@]}"
 	echo "Items: ${ARRAY[@]}"
+	echo "Indexies: ${!ARRAY[@]}"
 	for ((i=0; i<${#ARRAY[@]}; i++)); do echo "${ARRAY[$i]}"; done;
 }
 DestructiveOperation() {
 	local ARRAY=(A B C D E)
 	echo "Items: ${ARRAY[@]}"
+	echo "Indexies: ${!ARRAY[@]}"
 
 	# 指定位置の要素を削除する
 	unset ARRAY[2]
 	echo "Items: ${ARRAY[@]}"
+	echo "Indexies: ${!ARRAY[@]}"
 	# 先頭の要素を削除する
 	ARRAY=("${ARRAY[@]:1}")
 	echo "Items: ${ARRAY[@]}"
+	echo "Indexies: ${!ARRAY[@]}"
 	# 末尾の要素を削除する
 	declare -i num=${#ARRAY[@]}-1
 	ARRAY=("${ARRAY[@]:0:$num}")
 	echo "Items: ${ARRAY[@]}"
+	echo "Indexies: ${!ARRAY[@]}"
 	
 	# 先頭に追加
 	ARRAY=(9 "${ARRAY[@]}")
 	echo "Items: ${ARRAY[@]}"
+	echo "Indexies: ${!ARRAY[@]}"
 	# 末尾に追加
 	ARRAY=("${ARRAY[@]}" Y) 
 	echo "Items: ${ARRAY[@]}"
+	echo "Indexies: ${!ARRAY[@]}"
 	ARRAY+=( Z )  
 	echo "Items: ${ARRAY[@]}"
+	echo "Indexies: ${!ARRAY[@]}"
 
 	# 指定位置に代入
 	ARRAY[0]=7
